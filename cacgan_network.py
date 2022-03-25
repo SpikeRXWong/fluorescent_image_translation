4d02cb69107f1bc25203ed2d2dfca7e5250a7964
Ruixiong Wang <rw17789@bristol.ac.uk>
2022-03-25 12:45:04 +0000

    Add code comment

--- cacgan_network.py
+++ cacgan_network.py
@@ -24,6 +24,29 @@ def snconvtranspose2d(in_channels, out_channels, kernel_size, stride=1, padding=
 
 class GenUnit(nn.Module):
     def __init__(self, in_channels, out_channels, dtype, activation = 'relu', use_dropout = False, use_sn = True):
+        """
+        Basic unit to build GenBlock for Generator.
+        
+        Parameters
+        ----------
+        in_channels : int
+            number of input feature map channels.
+        out_channels : int
+            number of output feature map channels.
+        dtype : str
+            indicate the function of this unit, up-sampling, down-sampling or not change.
+        activation : str, optional
+            activation function, 'relu' for ReLU or 'leaky' for LeakyReLU(0.2). The default is 'relu'.
+        use_dropout : Bool, optional
+            whether add Dropout layer in the unit. The default is False.
+        use_sn : Bool, optional
+            whether use spectral normalization. The default is True.
+
+        Returns
+        -------
+        output feature maps.
+
+        """
         super(GenUnit, self).__init__()
         assert dtype in ['down', 'up', 'in']
         assert activation in ['relu', 'leaky']
@@ -61,10 +84,45 @@ class GenUnit(nn.Module):
         self.unit = nn.Sequential(*layerlist)
         
     def forward(self, x):
+        """
+        x : Tensor
+            Bs x in_channels x W x H.
+
+        Output:
+        -------
+        Tensor
+        type: 'down': Bs x out_channels x W/2 x H/2;
+              'in'  : Bs x out_channels x W x H;
+              'up'  : Bs x out_channels x w*2 x H*2.
+
+        """
         return self.unit(x)
     
 class GenBlock(nn.Module):
     def __init__(self, in_channels, out_channels, dtype, activation = 'relu', use_dropout = False, add_Attention = False, use_sn = True, middel_channels = None):
+        """
+        Module to build Generator, composed of GenUnit
+
+        Parameters
+        ----------
+        in_channels : int
+            number of input channels.
+        out_channels : int
+            number of output channels.
+        dtype : str
+            indicate the function of this unit, 'up': up-sampling, 'down': down-sampling or 'in': feature map size unchange.
+        activation : str, optional
+            activation function, 'relu' for ReLU or 'leaky' for LeakyReLU(0.2). The default is 'relu'.
+        use_dropout : Bool, optional
+            whether add Dropout layer in the unit. The default is False.
+        add_Attention : Bool, optional
+            For SACGAN indicate whether to add a self-attetion module after the model. The default is False.
+        use_sn : Bool, optional
+            whether use spectral normalization. The default is True.
+        middel_channels : int, optional
+            if not none. The default is None.
+
+        """
         super(GenBlock, self).__init__()
         assert dtype in ['down', 'up'], 'Block type should be down or up'
         assert activation in ['relu', 'leaky']
@@ -105,6 +163,18 @@ class GenBlock(nn.Module):
         self.attention = Self_Attention(in_channels if dtype == "down" else out_channels, use_sn = use_sn) if add_Attention else None
                 
     def forward(self, x):
+        """
+        x : Tensor
+            Bs x in_channels x W x H.
+
+        Output:
+        -------
+        Tensor
+        type: 'down': Bs x out_channels x W/2 x H/2;
+              'in'  : Bs x out_channels x W x H;
+              'up'  : Bs x out_channels x w*2 x H*2.
+
+        """
         if self.attention is None:
             return self.block(x) + self.residual(x)
         elif self.dtype == "down":
@@ -114,11 +184,31 @@ class GenBlock(nn.Module):
             return self.attention(self.block(x) + self.residual(x))
         
 class Generator(nn.Module):
-    """
-    Generator
-    input feature maps size shouble be 256
-    """
     def __init__(self, in_channels, out_channels, attn_pos='01110', has_mask = False, hidden_channels = 64, down_step = 5, bypass = False, use_sn = True):
+        """
+        Generator model
+
+        Parameters
+        ----------
+        in_channels : int
+            number of input bright-field image slices.
+        out_channels : int
+            number of output fluorescent image slices.
+        attn_pos : str, optional
+            a series of "0" or "1", "0" means no attention module inserted, vice versa. The default is '01110'.
+        has_mask : Bool, optional
+            whether include mask generation path in Generator, True for CACGAN, False for SACGAN. The default is False.
+        hidden_channels : int, optional
+            parameter for number of input and output channels of GenBlock. The default is 64.
+        down_step : int, optional
+            number of down-sampling number, choice in 4 or 5. The default is 5.
+        bypass : Bool, optional
+            type of cross-attention module, discard now. The default is False.
+        use_sn : Bool, optional
+            whether use spectral normalization. The default is True.
+
+        """
+        
         super(Generator, self).__init__()
         assert down_step in [4,5]
         assert len(attn_pos) >= down_step
@@ -202,6 +292,19 @@ class Generator(nn.Module):
             #     )
         
     def forward(self, x):
+        """
+        Input
+        ----------
+        x : Tensor [-1, 1]
+            Bs x in_channles x W x H.
+            W and H are 128 for training and evaluation and 256 for testing
+
+        Output
+        -------
+        out : Tensor [-1, 1]
+            Bs x output_channles x W x H.
+
+        """
         d = []
         for layer in self.down:
             d.append(layer(d[-1] if len(d) != 0 else x))
@@ -242,17 +345,24 @@ class Generator(nn.Module):
         F.relu(x - 0.5) / (torch.abs(x - 0.5) + 1e-8)
     
 class DisBlock(nn.Module):
-    """
-    Block for building Discriminator
-    in_channels: number of input channels
-    out_channels: number of output channels
-    All Block contains Convolutional layers with kernel_size=4 and padding=1
-    stride: stride for convlutional layers, can be 2(default) or 1
-    has_bn: add BatchNormalization
-    has_relu: add leaky RelU(0.2, True)
-    use_sn: whether to use spectral normalization
-    """
     def __init__(self, in_channels, out_channels, bias = False, add_Attention = False, use_sn = True):
+        """
+        Basic unit to build Discriminator
+
+        Parameters
+        ----------
+        in_channels : int
+            number of input channels.
+        out_channels : int
+            number of output channels.
+        bias : Bool, optional
+            whether to use bias in convolutional function layer. The default is False.
+        add_Attention : bool, optional
+            whetther to add self-attention module behind. The default is False.
+        use_sn : bool, optional
+            whether to use spectral normalization. The default is True.
+
+        """
         super(DisBlock, self).__init__()
         conv2d = snconv2d if use_sn else nn.Conv2d
         
@@ -271,6 +381,18 @@ class DisBlock(nn.Module):
         self.attention = Self_Attention(out_channels, use_sn = use_sn) if add_Attention else None
         
     def forward(self, x):
+        """
+        Input
+        ----------
+        x : Tensor
+            Bs x in_channels x W x H.
+
+        Output
+        -------
+        Tensor
+            Bs x out_channels x W/2 x H/2.
+
+        """
         x_r = self.down(self.residual(x))
         x0 = self.conv(x)
         out = x_r + x0
@@ -280,9 +402,30 @@ class DisBlock(nn.Module):
             return out
         
 class Discriminator(nn.Module):
-    def __init__(self, in_channels, hidden_channels = 64, attention_level= "Non", down_step = 5, use_sn = True, batch = True):
+    def __init__(self, in_channels, hidden_channels = 64, attention_level= "Non", use_sn = True, batch = True):
+        """
+        Discriminator model for conditional GAN
+
+        Parameters
+        ----------
+        in_channels : int
+            number of input channels.
+        hidden_channels : int, optional
+            parameters for number of input and ouput channel for each layer. The default is 64.
+        attention_level : str, optional
+            where to insert self-attention module, two position for self-attention module, after first down-sampling and after the second down-sampling. The default is "Non".
+        use_sn : bool, optional
+            whether to use spectral normalization. The default is True.
+        batch : bool, optional
+            whether to use PatchGAN. The default is True.
+
+        Returns
+        -------
+        None.
+
+        """
         super(Discriminator, self).__init__()
-        assert down_step in [4,5]
+        # assert down_step in [4,5]
         assert attention_level in [ 'non', 'Non', 'Top', 'top', 'Sec', 'sec', 'Two', 'two']
         pos = [False, False]
         if attention_level in ['Top', 'top', 'Two', 'two']:
@@ -314,6 +457,25 @@ class Discriminator(nn.Module):
                 )
         
     def forward(self, x, y=None):
+        """
+        Inputs
+        ----------
+        x : Tensor
+            if y is not none, x is input bright-field image.
+            else the image for discrimating
+        y : Tensor, optional
+            If not none, the fluorescent image for discriminating. The default is None.
+        shape: 
+            x: Bs x in_channel x 128 x 128, if y is none. else Bs x bright-field image channel x 128 x 128
+            y: Bs x fluorescent image channel x 128 x 128
+            if y is not none in_channel = bright-field image channel + fluorescent image channel
+
+        Outputs
+        -------
+        out : Tensor
+            if batch: Bs x 1 x 30 x 30, else Bs x 1 x 1 x 1
+            
+        """
         if y is None:
             input = x
         else:
